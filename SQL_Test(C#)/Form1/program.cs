9b75190171c621fd442bf306e9d42b79146bd6f4
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 20:18:40 +0200

    Create program.cs

---
+++ SQL_Test(C#)/Form1/program.cs
@@ -0,0 +1,20 @@
+sing System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
